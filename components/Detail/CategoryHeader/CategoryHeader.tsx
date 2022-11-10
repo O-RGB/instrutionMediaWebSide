b0266203a009c06a868e4ca038e32c0602d9e3d1
O-RGB <54633251+O-RGB@users.noreply.github.com>
2022-11-10 21:28:04 +0700

    gap

--- components/Detail/CategoryHeader/CategoryHeader.tsx
+++ components/Detail/CategoryHeader/CategoryHeader.tsx
@@ -21,7 +21,7 @@ const CategoryHeader: React.FC<ICategoryHeader> = ({
           <li>หมวดหมู่</li>
         </ul>
       </div>
-      <div className="p-2 py-3 text-xs text-white font-bold flex flex-wrap gap-4">
+      <div className="p-2 py-3 text-xs text-white font-bold flex flex-wrap gap-3">
         {tag.map((e,i) => {
           return (
             <React.Fragment key={`category-i-lop-${i}`}>
